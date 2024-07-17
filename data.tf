88ff4c79f25e68464fcb6e3e0621a1e94d92d573
Anonymoussaurus <50231698+AnonymousWP@users.noreply.github.com>
2024-07-17 14:58:52 +0200

    Fix typo in policy

--- data.tf
+++ data.tf
@@ -294,7 +294,7 @@ data "aws_iam_policy_document" "this" {
       condition {
         test     = "BoolIfExists"
         variable = "aws:MultiFactorAuthPresent"
-        values   = ["false, "]
+        values   = ["false"]
       }
     }
   }
